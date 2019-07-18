e5002146f2b6fc33d1f3d6f5fe27bb674c46480a
v-hub <v-hui@microsoft.com>
2019-07-18 18:03:31 +0800

    del

--- src/gelato/lib/util.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-module.exports = {
-  writeBashrc: (content) => `echo "${content}" >> ~/.bashrc && source ~/.bashrc`,
-  aptInstall: (...packages) => `apt-get install -y --no-install-recommends ${packages.join(' ')}`,
-  appendToFile: (content, file) => `echo "${content}" >> ${file}`
-}
